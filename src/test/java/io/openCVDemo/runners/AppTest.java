d33074e50e31b1c8ab64f9f9683db32947bbe12b
Maximiliano Gigena <maximiliano.gigena@outlook.com.ar>
2017-03-01 01:51:23 -0300

    Solving problem with m2e

--- src/test/java/io/openCVDemo/runners/AppTest.java
+++ src/test/java/io/openCVDemo/runners/AppTest.java
@@ -1,6 +1,7 @@
 package io.openCVDemo.runners;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = "src/test/resources/features/OpenApp.feature",
     glue = "io.openCVDemo.glue", format = {"pretty"})
